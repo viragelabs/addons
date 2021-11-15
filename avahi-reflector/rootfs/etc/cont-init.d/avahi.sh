05f05f110cb346fbd0ffcca303ca130e312ade58
Virage Laboratories <87444270+viragelabs@users.noreply.github.com>
2021-11-15 16:48:16 -0500

    Update avahi.sh

--- avahi-reflector/rootfs/etc/cont-init.d/avahi.sh
+++ avahi-reflector/rootfs/etc/cont-init.d/avahi.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Home Assistant Add-on: Avahi Daemon
-# Configures Avahi-Daemon
+# Home Assistant Add-on: Avahi Daemon for MQTT
+# Configures Avahi-Daemon to broadcast MQTT Service
 # ==============================================================================
 readonly AVAHI_CONF='/etc/avahi/avahi-daemon.conf'
 declare HOSTNAME
 declare INTERFACE
 
-#HOSTNAME=$(bashio::config 'Hostname')
+#HOSTNAME=$(bashio::config 'hostname')
 #if bashio::var.is_empty "${HOSTNAME}";
-if bashio::config.is_empty 'Hostname'; then
+if bashio::config.is_empty 'hostname'; then
     bashio::log.warning "Can't read hostname, using default."
-		HOSTNAME="hassio"
+		HOSTNAME="viragehub"
 else
-    HOSTNAME=$(bashio::config 'Hostname')
+    HOSTNAME=$(bashio::config 'hostname')
 fi
 
-#INTERFACE=$(bashio::config 'Interface')
+#INTERFACE=$(bashio::config 'interface')
 #if bashio::var.is_empty "${INTERFACE}";
-if bashio::config.is_empty 'Interface'; then
+if bashio::config.is_empty 'interface'; then
     bashio::log.warning "Can't read interface, using default."
 		INTERFACE="eth0"
 else
-    INTERFACE=$(bashio::config 'Interface')
+    INTERFACE=$(bashio::config 'interface')
 fi
 
 
